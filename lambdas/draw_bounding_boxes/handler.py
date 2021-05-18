809925d2349cb0b36b33e28d14c2543e77a88534
Joonas Pessi <joonas.pessi@gmail.com>
2021-05-18 20:23:33 +0300

    store bb labeled image to s3

--- lambdas/draw_bounding_boxes/handler.py
+++ lambdas/draw_bounding_boxes/handler.py
@@ -1,18 +1,24 @@
 import io
+from os import path
 
 import boto3
 from aws_lambda_powertools import Logger, Tracer
-from PIL import ExifTags, Image, ImageColor, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 
 logger = Logger()
 tracer = Tracer()
 
 
-def _s3_client():
+def _s3_resource():
     return boto3.resource("s3")
 
 
-s3 = _s3_client()
+def _s3_client():
+    return boto3.client("s3")
+
+
+s3_resource = _s3_resource()
+s3_client = _s3_client()
 
 
 @tracer.capture_lambda_handler
@@ -22,30 +28,50 @@ def handler(event, _):
     keys = event["ImageKeysResult"]["Payload"]
 
     for key, labels in zip(keys, labels):
-        s3_object = s3.Object(key["bucket_name"], key["object_key"])
+        s3_object = s3_resource.Object(key["bucket_name"], key["object_key"])
         s3_response = s3_object.get()
         stream = io.BytesIO(s3_response["Body"].read())
         image = Image.open(stream)
-        imgWidth, imgHeight = image.size
-        draw = ImageDraw.Draw(image)
 
-        for label in labels["Labels"]:
-            for instance in label["Instances"]:
-                box = instance["BoundingBox"]
-                left = imgWidth * box["Left"]
-                top = imgHeight * box["Top"]
-                width = imgWidth * box["Width"]
-                height = imgHeight * box["Height"]
+        image_with_labels = _draw_labels(image, labels)
 
-                points = (
-                    (left, top),
-                    (left + width, top),
-                    (left + width, top + height),
-                    (left, top + height),
-                    (left, top),
-                )
+        bucket = key["bucket_name"]
+        image_id = path.basename(key["object_key"])
+        _save_image_to_s3(
+            image_with_labels, bucket=bucket, key=path.join("labeled", image_id)
+        )
 
-                draw.line(points, fill="#00d400", width=2)
-                draw.text((left, top), f'{label["Name"]} : {label["Confidence"]}')
 
-        image.show()
+def _draw_labels(input_image: Image, labels) -> Image:
+    image = input_image.copy()
+    image.format = input_image.format
+    imgWidth, imgHeight = image.size
+    draw = ImageDraw.Draw(image)
+
+    for label in labels["Labels"]:
+        for instance in label["Instances"]:
+            box = instance["BoundingBox"]
+            left = imgWidth * box["Left"]
+            top = imgHeight * box["Top"]
+            width = imgWidth * box["Width"]
+            height = imgHeight * box["Height"]
+
+            points = (
+                (left, top),
+                (left + width, top),
+                (left + width, top + height),
+                (left, top + height),
+                (left, top),
+            )
+
+            draw.line(points, fill="#00d400", width=2)
+            draw.text((left, top), f'{label["Name"]} : {label["Confidence"]}')
+    return image
+
+
+def _save_image_to_s3(image: Image, bucket, key):
+    image.show()
+    in_mem_file = io.BytesIO()
+    image.save(in_mem_file, format=image.format)
+    in_mem_file.seek(0)
+    s3_client.upload_fileobj(in_mem_file, Bucket=bucket, Key=key)
--- tests/unit/test_draw_bounding_boxes.py
+++ tests/unit/test_draw_bounding_boxes.py
@@ -1,11 +1,19 @@
+import botocore
+
+
+def s3_object_exists(bucket, key):
+    import boto3
+
+    s3 = boto3.resource("s3")
+    try:
+        s3.Object(bucket, key).load()
+    except botocore.exceptions.ClientError:
+        return False
+    return True
+
+
 def test_draw_bounding_boxes(bucket, draw_bb_event, lambda_context):
     from lambdas.draw_bounding_boxes import handler
 
-    response = handler.handler(draw_bb_event, lambda_context)
-
-    assert response == [
-        {
-            "bucket_name": "whatsinimagestack-whatsinimagesbucketf5245105-nmx1ow78uj0r",
-            "object_key": "images/bike.png",
-        }
-    ]
+    handler.handler(draw_bb_event, lambda_context)
+    assert s3_object_exists("testing", "labeled/01F5TK7AZHGYHMHBD1C299496F.png") is True
